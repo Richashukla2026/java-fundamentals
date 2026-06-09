4bdb89cc45d2438c2bc5e0834ef2380943ad0676
Richa Shukla <rashu201487@gmail.com>
2026-06-10 00:01:27 +0200

    ExerciseOne.java file to be commited

---
+++ src/main/java/ExerciseOne.java
@@ -0,0 +1,22 @@
+public class ExerciseOne {
+    void  main() {
+        //Step 1 Declaration of Variables
+        String name;
+        int age;
+        String city;
+        //Step 2 Assignment of values to Variables
+        name = "Sofia";
+        age =  22;
+        city = "Stockholm";
+        //Step 3 Print the output
+        IO.println("====================");
+        IO.println("     My Profile     ");
+        IO.println("====================");
+        IO.println("Name : " + name);
+        IO.println("Age  : " + age);
+        IO.println("city : " + city);
+        IO.println("====================");
+
+    }
+
+}
