48930407a0d8a6a973b5bdc3a43ccd9d049f9aae
Richa Shukla <rashu201487@gmail.com>
2026-06-10 23:36:22 +0200

    Exercise two is ready to be run

---
+++ src/main/java/ExerciseTwo.java
@@ -0,0 +1,25 @@
+import javax.naming.Name;
+import java.util.Scanner;
+
+public class ExerciseTwo {
+    static void main() {
+        //Declare variable
+        int year;
+
+        //Assign value
+        year = 2024;
+        //input variables
+        Scanner scanner1 = new Scanner(System.in);
+
+        System.out.print("Enter your year: ");
+        year = scanner1.nextInt();
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+        // Step 5: Close the scanner
+        scanner1.close();
+    }
+}
